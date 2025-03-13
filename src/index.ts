005351b4c294da74986fcf722579b3a8961bae92
Jeremy Holstein <jjjrmy@gmail.com>
2025-03-12 23:33:51 -0400

    port?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import { ExpressAuthMiddleware } from "@sdijeremy/shared";
 
+const port = process.env.PORT || 3000
+
 const app = express();
 
 // Routes
@@ -12,3 +14,7 @@ app.get("/auth-test", ExpressAuthMiddleware, (req: Request, res: Response) => {
   const user = res.locals.user;
   res.json({ message: "Hello NodeJS!", user });
 });
+
+app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
+
+export default app;
